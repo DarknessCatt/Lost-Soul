8d10f4a9f83805418371b238659a2a23e3755aa3
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-03-24 11:40:06 -0300

    Hitboxes and Hurtboxes

---
+++ Lost_Soul/Entities/Combat/Hitbox/Hitbox.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+enum IDS {None, Player, Enemies, Hazard, Other}
+
+export(NodePath) var Body : NodePath
+onready var body_node : Node2D = get_node(Body)
+
+export(IDS) var Id : int = 0
+export(int) var Damage : int = 0
+export(int) var Force : int = 0
+export(Vector2) var Direction : Vector2 = Vector2(0,0) setget , get_direction
+
+func get_direction():
+	if body_node.scale.x == 1:
+		return Direction
+
+	else:
+		return Vector2(-Direction.x, Direction.y)
