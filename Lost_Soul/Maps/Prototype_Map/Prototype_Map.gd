880275ccc0a3055e7750c1d7cdd9f3624391fbae
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-03-31 11:17:37 -0300

    Ending Dead World Proto Map

--- Lost_Soul/Maps/Prototype_Map/Prototype_Map.gd
+++ Lost_Soul/Maps/Prototype_Map/Prototype_Map.gd
@@ -3,37 +3,59 @@ extends Node2D
 const church_camera_pos : Vector2 = Vector2(-4690,-250)
 const church_camera_zoom : Vector2 = Vector2(6,6)
 
-enum {NONE, LINE, JUMP, CHURCH}
+enum {NONE, INTRO, LINE, JUMP, CHURCH}
 var STATE = NONE
 
+var intro_lines : Array = [ "Sua morte deve\nter sido traumatica...            ",
+							"\nVocê está no mundo dos mortos agora.       ",
+							"\nSiga para [->]\nonde está a fila da ressureição."
+							]
 var line_lines : Array = ["Então, é isso?\n                    ",
 						 "Vai desistir\nfacil assim?\n                    ",
-						 "Acredito que você\nainda tenha\nassuntos pendentes.\n                    ",
+						 "Acredito que \ntenhas assuntos\ninacabados.\n                    ",
 						 "Eu posso te\najudar a voltar.\n                    ",
 						 "Saia desta fila,\nme encontre.\n                   "]
-var jump_lines : Array = ["Eu aprecio a\ntua determinação.\n                    ",
-						 "Mas você ainda não\nestá acostumado\ncom este corpo.\n                    ",
-						 "Foque na tua mente\npara pular."]
+var jump_lines : Array = ["Concentre tua mente\npara pular."]
 
 var current_lines : Array
 var line_num : int = 0
 
 func _on_Textbox_ended():
 	if line_num < len(current_lines):
-		$Hero/Textbox.change_text(current_lines[line_num])
+		if STATE == INTRO:
+			$Characters/DeadManager/Textbox.change_text(current_lines[line_num])
+		else:
+			$Hero/Textbox.change_text(current_lines[line_num])
 		line_num += 1
 	else:
 		if STATE == JUMP:
 			$Tween.stop_all()
 			$Tween.interpolate_property($Jump/Z, "color:a", 0, 0.8, 4, Tween.TRANS_SINE, Tween.EASE_IN)
 			$Tween.start()
+		elif STATE == INTRO:
+			STATE = NONE
+
+func _ready():
+	STATE = INTRO
+	$Hero.on_cutscene = true
+	$Hero._change_anim("StandingUp")
+	$Tween.interpolate_property($Hero/Camera, "zoom", Vector2(0.3,0.3), Vector2(1,1), 10, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	$Tween.interpolate_property($Hero/Camera, "position", Vector2(0, 0), Vector2(0, -75), 10, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($Characters/DeadManager/Textbox, "rect_position", Vector2(-24, 73), Vector2(-200, -151), 15, Tween.TRANS_EXPO)
+	$Tween.start()
+
+	current_lines = intro_lines
+	$Intro.start()
+
+func _on_Intro_timeout():
+	$Hero.on_cutscene = false
 
 func _on_Line_body_entered(_body):
 	STATE = LINE
 
+	line_num = 0
 	current_lines = line_lines
-	$Hero/Textbox.change_text(current_lines[line_num])
-	line_num += 1
+	$Hero/Textbox.change_text("          \n...         \n          \n")
 
 	$Tween.stop_all()
 	$Tween.interpolate_property($Hero/Camera, "zoom", Vector2(1,1), Vector2(0.6,0.6), 5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
@@ -53,6 +75,7 @@ func _on_Line_body_exited(_body):
 
 func _on_Jump_body_entered(_body):
 	STATE = JUMP
+	line_num = 0
 	current_lines = jump_lines
 	$Hero/Textbox.change_text(current_lines[line_num])
 	line_num += 1
