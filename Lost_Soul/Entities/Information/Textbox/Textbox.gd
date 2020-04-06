5324f49f91a30f9af5fb33d4f9502c6f4e313768
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-04-06 14:18:58 -0300

    Textbox Refactor

--- Lost_Soul/Entities/Information/Textbox/Textbox.gd
+++ Lost_Soul/Entities/Information/Textbox/Textbox.gd
@@ -2,14 +2,16 @@ extends Label
 
 export(bool) var Autostart : bool = false
 export(float) var Letter_Delay : float = 0.1
+export(float) var Sentence_Delay : float = 1
+export(Array, String, MULTILINE) var dialogue : Array = []
 
-var text_to_print : String = ""
+var text_to_print : int = 0
 var character_num : int = 0
 
 signal ended()
 
 func _ready():
-	text_to_print = self.text
+	text_to_print = 0
 	self.text = ""
 
 	$letterSpeed.wait_time = Letter_Delay
@@ -20,21 +22,32 @@ func _ready():
 func begin() -> void:
 	$letterSpeed.start()
 
-func change_text(new_text : String) -> void:
-	self.text = ""
-	text_to_print = new_text
+func change_dialogue(new_dial : Array) -> void:
+	dialogue = new_dial
 	character_num = 0
-	begin()
+	self.clear()
 
 func clear() -> void:
 	self.text = ""
+	text_to_print = 0
 	character_num = 0
 	$letterSpeed.stop()
+	$sentenceSpeed.stop()
 
 func _on_letterSpeed_timeout():
 	character_num += 1
-	self.text = text_to_print.left(character_num)
+	self.text = dialogue[text_to_print].left(character_num)
 
-	if character_num == text_to_print.length():
+	if character_num == dialogue[text_to_print].length():
+		text_to_print += 1
 		$letterSpeed.stop()
-		emit_signal("ended")
+
+		if text_to_print == dialogue.size():
+			emit_signal("ended")
+
+		else:
+			$sentenceSpeed.start()
+
+func _on_sentenceSpeed_timeout():
+	character_num = 0
+	$letterSpeed.start()
--- Lost_Soul/Maps/Prototype_Dead_World/Prototype_Dead_World.gd
+++ Lost_Soul/Maps/Prototype_Dead_World/Prototype_Dead_World.gd
@@ -3,88 +3,45 @@ extends Node2D
 const church_camera_pos : Vector2 = Vector2(-4690,-250)
 const church_camera_zoom : Vector2 = Vector2(6,6)
 
-enum {NONE, INTRO, LINE, JUMP, CHURCH}
-var STATE = NONE
-
-var intro_lines : Array = [ "Sua morte deve\nter sido traumatica...            ",
-							"\nVocê está no mundo dos mortos agora.       ",
-							"\nSiga para [->]\nonde está a fila da ressureição."
-							]
-var line_lines : Array = ["Então, é isso?\n                    ",
-						 "Vai desistir\nfacil assim?\n                    ",
-						 "Acredito que \ntenhas assuntos\ninacabados.\n                    ",
-						 "Eu posso te\najudar a voltar.\n                    ",
-						 "Saia desta fila,\nme encontre.\n                   "]
-var jump_lines : Array = ["Concentre tua mente\npara pular."]
-
-var current_lines : Array
-var line_num : int = 0
-
-func _on_Textbox_ended():
-	if line_num < len(current_lines):
-		if STATE == INTRO:
-			$Characters/DeadManager/Textbox.change_text(current_lines[line_num])
-		else:
-			$Hero/Textbox.change_text(current_lines[line_num])
-		line_num += 1
-	else:
-		if STATE == JUMP:
-			$Tween.stop_all()
-			$Tween.interpolate_property($Jump/Z, "color:a", 0, 0.8, 4, Tween.TRANS_SINE, Tween.EASE_IN)
-			$Tween.start()
-		elif STATE == INTRO:
-			STATE = NONE
-
 func _ready():
-	STATE = INTRO
 	$Hero.on_cutscene = true
 	$Hero._change_anim("StandingUp")
 	$Tween.interpolate_property($Hero/Camera, "zoom", Vector2(0.3,0.3), Vector2(1,1), 10, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.interpolate_property($Hero/Camera, "position", Vector2(0, 0), Vector2(0, -75), 10, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-	$Tween.interpolate_property($Characters/DeadManager/Textbox, "rect_position", Vector2(-24, 73), Vector2(-200, -151), 15, Tween.TRANS_EXPO)
+	$Tween.interpolate_property($Characters/DeadManager/Manager_Text, "rect_position", Vector2(-24, 73), Vector2(-200, -151), 15, Tween.TRANS_EXPO)
 	$Tween.start()
 
-	current_lines = intro_lines
-	$Intro.start()
+	$Characters/DeadManager/Manager_Text.begin()
 
-func _on_Intro_timeout():
+func _on_Manager_Text_ended():
 	$Hero.on_cutscene = false
 
 func _on_Line_body_entered(_body):
-	STATE = LINE
-
-	line_num = 0
-	current_lines = line_lines
-	$Hero/Textbox.change_text("          \n...         \n          \n")
-
 	$Tween.stop_all()
 	$Tween.interpolate_property($Hero/Camera, "zoom", Vector2(1,1), Vector2(0.6,0.6), 5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($BG, "modulate:a", 0, 0.8, 7, Tween.TRANS_SINE, Tween.EASE_IN)
 	$Tween.start()
 
+	$Line_Dialogue.begin()
+
 func _on_Line_body_exited(_body):
-	STATE = NONE
-
-	$Hero/Textbox.clear()
-	line_num = 0
-
 	$Tween.stop_all()
 	$Tween.interpolate_property($Hero/Camera, "zoom", $Hero/Camera.zoom, Vector2(1,1), 1, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($BG, "modulate:a", $BG.modulate.a, 0, 1, Tween.TRANS_SINE, Tween.EASE_IN)
 	$Tween.start()
 
+	$Line_Dialogue.clear()
+
 func _on_Jump_body_entered(_body):
-	STATE = JUMP
-	line_num = 0
-	current_lines = jump_lines
-	$Hero/Textbox.change_text(current_lines[line_num])
-	line_num += 1
+	$Jump_Dialogue.begin()
+
+func _on_Jump_Dialogue_ended():
+	$Tween.stop_all()
+	$Tween.interpolate_property($Jump/Z, "color:a", 0, 0.8, 3, Tween.TRANS_SINE, Tween.EASE_IN)
+	$Tween.start()
 
 func _on_Jump_body_exited(_body):
-	STATE = NONE
-	$Hero/Textbox.clear()
-	line_num = 0
-
+	$Jump_Dialogue.clear()
 	$Jump/Z.color.a = 0
 
 func _on_Church_body_entered(_body):
@@ -103,7 +60,3 @@ func _on_Church_body_exited(_body):
 	$Tween.interpolate_property($Hero/Camera, "position", $Hero/Camera.position, Vector2(0, -75), 2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Hero/Camera, "zoom", $MapCamera.zoom, Vector2(1,1), 2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.start()
-
-func _on_Tween_tween_all_completed():
-	if STATE == LINE:
-		$Hero/Textbox.begin()
