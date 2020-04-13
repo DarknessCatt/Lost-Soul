b10485c8073bfb8b296caa1fd6f17a4a2f478791
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-04-13 16:45:59 -0300

    Boss Enemy

---
+++ Lost_Soul/Entities/Boss/FSM/Attack.gd
@@ -0,0 +1,57 @@
+extends "EnemyState.gd"
+
+var attack_ended : bool = false
+
+#Movement
+const NORMAL : Vector2 = Vector2(0, -1)
+
+const GRAV : int = 3500
+const MAX_GRAV : int = 1500
+
+const FRICTION : float = 0.7
+
+#Moving Attacks
+var attack_dir : float = 0.0
+var speed : Vector2 = Vector2(0, 10)
+
+func impulse(direction : Vector2):
+	self.speed = Vector2(direction.x*attack_dir, direction.y)
+
+func enter(Enemy : KinematicBody2D) -> void:
+	Enemy.speed = Vector2(0,0)
+	self.speed = Vector2(0,0)
+	attack_ended = false
+
+	attack_dir = Enemy.body.scale.x
+
+	if (Enemy.position.y -  Enemy.Player.position.y) > 30 or rand_range(0, 1) >= 0.7:
+		Enemy._change_anim("UpTilt")
+		$UpTilt.start()
+	else:
+		Enemy._change_anim("Jab")
+		$Jab.start()
+
+func exit(_Enemy : KinematicBody2D) -> void:
+	_Enemy._clear_attack_polys()
+	_Enemy._disable_hitboxes()
+
+func update(_Enemy: KinematicBody2D, delta : float) -> void:
+
+	if attack_ended:
+		_Enemy._change_state($"../OnCombat")
+
+	if speed.x != 0:
+		speed.x *= FRICTION
+		if abs(speed.x) < 10 : speed.x = 0
+
+	_Enemy.move_and_slide(speed, NORMAL)
+
+	if _Enemy.is_on_floor():
+		speed.y = 10
+
+	else:
+		speed.y += GRAV*delta
+		if speed.y > MAX_GRAV: speed.y = MAX_GRAV
+
+func _on_Attack_timeout():
+	attack_ended = true
---
+++ Lost_Soul/Entities/Boss/FSM/OnCombat.gd
@@ -0,0 +1,98 @@
+extends "EnemyState.gd"
+
+#Player Info
+var player_in_range : bool = false
+
+#Movement
+const NORMAL : Vector2 = Vector2(0, -1)
+
+const GRAV : int = 3500
+const MAX_GRAV : int = 1500
+
+const ACCEL : int = 900
+const MAX_SPEED : int = 500
+const FRICTION : float = 0.8
+
+#Combat
+var min_dist : int
+var max_dist : int
+var attack_dist : int
+var attack_timer : float
+
+var attack_ready : bool = false
+
+##Functions
+func _on_Attack_Ready():
+	attack_ready = true
+
+#Direction
+enum {LEFT = -1, NONE, RIGHT}
+var direction : int = NONE
+
+func enter(Enemy : KinematicBody2D) -> void:
+	min_dist = 130 + int(rand_range(-10,+40))
+	max_dist = 180 + int(rand_range(-20,+20))
+	attack_dist = 200 + int(rand_range(-10,+10))
+
+	if Enemy.speed.x == 0:
+		Enemy._change_anim("OnCombat")
+	else:
+		Enemy._change_anim("Walking")
+
+	var player_dist = (Enemy.Player.position - Enemy.position).x
+	if sign(player_dist) != sign(Enemy.body.scale.x):
+		Enemy.body.scale.x *= -1
+
+	$Attack_Timer.wait_time = 0.5 + rand_range(-0.3, +0.3)
+	$Attack_Timer.start()
+
+func exit(_Enemy : KinematicBody2D) -> void:
+	$Attack_Timer.stop()
+
+func update(_Enemy : KinematicBody2D, delta : float) -> void:
+
+	var player_dist = (_Enemy.Player.position - _Enemy.position).x
+
+	if abs(player_dist) <= min_dist: player_in_range = true
+	elif abs(player_dist) >= max_dist: player_in_range = false
+
+	var spdx : float = _Enemy.speed.x
+
+	if player_in_range and sign(player_dist) == sign(_Enemy.body.scale.x):
+		direction = NONE
+
+	else:
+		direction = sign(player_dist)
+		spdx += direction*ACCEL*delta
+
+	if sign(spdx) != direction : spdx *= FRICTION
+
+	if abs(spdx) > MAX_SPEED: spdx = MAX_SPEED*direction
+	elif abs(spdx) < 4: spdx = 0
+
+	if sign(spdx) != sign(_Enemy.speed.x):
+		match sign(spdx):
+			-1.0:
+				_Enemy.body.scale.x = -1
+				_Enemy._change_anim("Walking")
+			0.0:
+				_Enemy._change_anim("OnCombat")
+			1.0:
+				_Enemy.body.scale.x = 1
+				_Enemy._change_anim("Walking")
+
+	_Enemy.speed.x = spdx
+
+	if _Enemy.is_on_floor():
+		_Enemy.speed.y = 10
+
+	else:
+		_Enemy.speed.y += GRAV*delta
+		if _Enemy.speed.y > MAX_GRAV: _Enemy.speed.y = MAX_GRAV
+
+	_Enemy.move_and_slide(_Enemy.speed, NORMAL)
+
+	if attack_ready and abs(player_dist) <= attack_dist:
+		attack_ready = false
+		_Enemy._change_state($"../Attack")
+
