cf922558c7aa3cd1c87c550479c84cf91f46159f
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-04-15 13:43:26 -0300

    Ending Cutscene and prototype finish

--- Lost_Soul/Maps/Prototype_Mountain/Prototype_Mountain.gd
+++ Lost_Soul/Maps/Prototype_Mountain/Prototype_Mountain.gd
@@ -18,22 +18,47 @@ func _on_Checkpoint_checkpoint_reached(checkpoint : Area2D):
 		enemy._respawn()
 	$Hazards/Arena._restart()
 
-var attack_dialogue : Array = ["Teu corpo ainda\nesta fraco.",
+var attack_dialogue : Array = ["Teu corpo ainda\nestá fraco.",
 							  "Eu irei te\nconceder um pouco\ndo meu poder.",
 							  ""]
 
-func _on_Attacking_Tutorial_entered(area):
+func _on_Attacking_Tutorial_entered(_body):
 	$Hero/Camera2D/Chaos.change_dialogue(attack_dialogue)
 	$Hero/Camera2D/Chaos.begin()
 	$Events/Attacking_Tutorial.call_deferred("set", "monitoring", false)
 	$Cutscene.play("Attack_Tutorial")
 
-var checkpoint_dialogue : Array = ["Ha um altar\nproximo.",
+var checkpoint_dialogue : Array = ["Há um altar\npróximo.",
 								 "Encoste nele...",
 								 "Eu posso manipular\nas suas energias.",
 								 ""]
 
-func _on_Checkpoint_Tutorial_entered(body):
+func _on_Checkpoint_Tutorial_entered(_body):
 	$Hero/Camera2D/Chaos.change_dialogue(checkpoint_dialogue)
 	$Hero/Camera2D/Chaos.begin()
 	$Events/Checkpoint_Tutorial.call_deferred("set", "monitoring", false)
+
+var heart_dialogue : Array = ["             ",
+						  "Impressionante...",
+						  "Um coração\nde cristal...",
+						  "Consuma suas energias,\n revigore tua alma.",
+						  ""]
+
+func _on_Crystal_Heart_collected(_body):
+	$Hero/Camera2D/Chaos.change_dialogue(heart_dialogue)
+	$Hero/Camera2D/Chaos.begin()
+	$Cutscene.play("Crystal_Tutorial")
+
+var ending_dialogue : Array = ["...",
+							 "Muito bem,",
+							 "fostes muito bem,",
+							 "Mas temos muito\no que fazer ainda...",
+							 "Este mundo\nserá meu.",
+							 "E tu me\najudaras,",
+							 "minha cara,",
+							 "Alma\nPerdida                                    \nPrototipo 1\n\nObrigado por jogar!"]
+
+func _on_Ending(_body):
+	$Hero/Camera2D/Chaos.change_dialogue(ending_dialogue)
+	$Hero/Camera2D/Chaos.begin()
+	$Cutscene.play("Ending")
