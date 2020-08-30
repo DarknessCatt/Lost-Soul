dd96b74d8baa01bc85f2106a6474168ec7c51f2a
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-08-30 14:37:52 -0300

    Finish Menus

---
+++ Lost_Soul/Menus/Credits/Credits.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _on_Voltar():
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Menus/Main_Menu/Main_Menu.tscn")
--- Lost_Soul/Menus/Main_Menu/Main_Menu.gd
+++ Lost_Soul/Menus/Main_Menu/Main_Menu.gd
@@ -1,17 +1,22 @@
 extends Control
 
 export(String) var game_scene : String
-export(String) var settings_scene : String
 export(String) var credits_scene : String
 
 func _on_New_Game_pressed():
 	get_tree().change_scene(game_scene)
-
-func _on_Settings_pressed():
-	get_tree().change_scene(settings_scene)
 
 func _on_Credits_pressed():
 	get_tree().change_scene(credits_scene)
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_Settings_pressed():
+	$Menu.hide()
+	$Settings.show()
+
+func _on_Settings_exited():
+	$Menu.show()
+	$Settings.hide()
