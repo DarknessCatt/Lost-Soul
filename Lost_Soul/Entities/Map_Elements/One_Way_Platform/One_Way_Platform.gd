9af1c802a7b1946a4fa86da6225ea9f9c438ecc4
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-09-13 11:09:28 -0300

    Add One Way Plataform

---
+++ Lost_Soul/Entities/Map_Elements/One_Way_Platform/One_Way_Platform.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+var Player = null
+
+func _on_player_on_top(body):
+	Player = body
+	if Input.is_action_pressed("hero_down"):
+		$Drop_Timer.start()
+
+func _on_player_exited(body):
+	Player = null
+	$Drop_Timer.stop()
+
+func _input(event):
+	if Player != null:
+		if event.is_action_pressed("hero_down"):
+			$Drop_Timer.start()
+
+		elif event.is_action_released("hero_down"):
+			$Drop_Timer.stop()
+
+func _on_Drop_timeout():
+	Player.position. y += 1
