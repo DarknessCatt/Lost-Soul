9c05b51fb2a3e0bf16be1ff2103e981fdd4d0d78
Matheus Lima Cunha <matheus.limacunha@gmail.com>
2020-11-09 18:20:16 -0200

    Small changes to rooms

---
+++ Lost_Soul/Maps/Procedural_Maps/Mountain/Bonus_Room/Bonus_Room.gd
@@ -0,0 +1,14 @@
+extends Base_Room
+
+signal change_tutorial(scene)
+
+func _on_exit_entered(_body, id):
+	self.emit_signal("player_exited", id)
+
+func open_exits(exits : Array, rank : int):
+	$Blocks.show()
+	for exit_data in exits: $Blocks.remove_child($Blocks.get_node(str(exit_data.exit.id)))
+	$Soul.Soul_Value = 100 + rank*50
+
+func get_spawn_point(exit_id : int) -> Vector2:
+	return $Entrances.get_child(exit_id).position
